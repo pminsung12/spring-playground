f1c10284abb89918b6a0fa0c1f58ff7e544bc869
Minseong Park <pminsung12@gmail.com>
2024-06-26 01:54:41 +0900

    feat: User entity

---
+++ src/main/java/org/example/demo/domain/user/User.java
@@ -0,0 +1,21 @@
+package org.example.demo.domain.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+}
