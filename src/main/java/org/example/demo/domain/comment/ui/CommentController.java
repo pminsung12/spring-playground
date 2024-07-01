e46f3ce0ca82fa3ca3a3152a402d8c3c95b862b2
Minseong Park <pminsung12@gmail.com>
2024-07-01 18:47:25 +0900

    feat(comment): swagger 업데이트 + 코멘트 생성 기능 추가

--- src/main/java/org/example/demo/domain/comment/ui/CommentController.java
+++ src/main/java/org/example/demo/domain/comment/ui/CommentController.java
@@ -1,14 +1,20 @@
 package org.example.demo.domain.comment.ui;
 
+import io.swagger.v3.oas.annotations.Operation;
+import org.example.demo.domain.post.Post;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.example.demo.domain.comment.Comment;
 import org.example.demo.domain.comment.CommentService;
 import org.example.demo.domain.post.PostService;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/comments")
+@Tag(name = "댓글")
 public class CommentController {
     private final CommentService commentService;
     private final PostService postService;
@@ -18,24 +24,34 @@ public class CommentController {
         this.postService = postService;
     }
 
-//    @PostMapping("/posts/{postId}")
-//    public Comment createComment(@PathVariable Long postId, @RequestBody Comment comment) {
-//        // comment 만들기
-//        Comment newComment = commentService.save(comment);
-//        return newComment;
-//    }
+    @PostMapping("/posts/{postId}")
+    public Comment createComment(@PathVariable Long postId, @RequestBody Comment comment) {
+        Post post = postService.findById(postId);
+        if (post == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Post not found");
+        }
+
+        comment.setPost(post);
+        return  commentService.save(comment);
+    }
 
     @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @Operation(summary = "댓글 삭제")
     public void deleteComment(@PathVariable Long id) {
         commentService.deleteById(id);
     }
 
     @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "댓글 조회")
     public Comment getCommentById(@PathVariable Long id) {
         return commentService.findById(id);
     }
 
     @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "댓글 목록 조회")
     public List<Comment> getAllComments() {
         return commentService.findAll();
     }
